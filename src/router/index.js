a5cf87bb986487edcdf0b8c2580d8242d71568e4
Matthew Reichardt <me@matthewreichardt.com>
2021-01-01 01:14:09 +0900

    Added router title and meta data

--- src/router/index.js
+++ src/router/index.js
@@ -6,22 +6,38 @@ const routes = [
   {
     path: "/",
     name: "home",
-    component: Home
+    component: Home,
+    meta: {
+      title: "Zano Life - Private Marketplace"
+    }
   },
   {
     path: "/login",
     name: "login",
-    component: () => import("../views/login.vue")
+    component: () => import("../views/login.vue"),
+    meta: {
+      title: "Login | Zano Life - Private Marketplace"
+    }
   },
   {
     path: "/register",
     name: "register",
-    component: () => import("../views/register.vue")
+    component: () => import("../views/register.vue"),
+    meta: {
+      title: "Register | Zano Life - Private Marketplace"
+    }
   }
 ];
+
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes
 });
+
+router.beforeEach((to, from, next) => {
+  window.document.title = to.meta.title;
+  next()
+})
+
 export default router;
