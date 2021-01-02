1bd425b5d597365a78a17a0c7f24c1fb453a2a76
Matthew Reichardt <me@matthewreichardt.com>
2021-01-03 00:08:43 +0900

    Listing via browser now works, minus file upload and categories

---
+++ src/views/NotFound.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <navigation />
+    <h1>Oops, it looks like the page you're looking for doesn't exist.</h1>
+  </div>
+</template>
+<script>
+  import Navigation from "@/components/shared/navigation";
+
+  export default {
+    components: {
+      Navigation
+    }
+  }
+</script>
